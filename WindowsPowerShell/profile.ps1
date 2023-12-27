4f5a243c3eb83cfaac9bd005784feef71b13d0c8
tiesen243 <ttien56906@gmail.com>
2023-12-28 06:13:24 +0700

    aaaa

--- WindowsPowerShell/profile.ps1
+++
@@ -1,8 +0,0 @@
-
-#region conda initialize
-# !! Contents within this block are managed by 'conda init' !!
-If (Test-Path "C:\Users\ttien\scoop\apps\miniconda3\current\Scripts\conda.exe") {
-    (& "C:\Users\ttien\scoop\apps\miniconda3\current\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
-}
-#endregion
-
